74b62a02dce1d42aa54c3eb137967051b729a9d1
Hafsa <hafsaahmad00900@gmail.com>
2025-07-11 18:57:27 +0500

    Initial Commit

---
+++ Birthday App/Birthday_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_AppApp.swift
+//  Birthday App
+//
+//  Created by Hafsa Ahmad on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
