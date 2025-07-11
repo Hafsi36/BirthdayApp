50d8ee7119ca0c9b0fddcba54d834b3788835baa
Hafsa <hafsaahmad00900@gmail.com>
2025-07-11 20:35:01 +0500

    finished setting up the UI

--- Birthday App/ContentView.swift
+++ Birthday App/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Dani", birthday: .now),
+        Friend(name: "Hani", birthday: .now)
+    ]
+    
+    @State private var newName: String = ""
+    @State private var newBirthday: Date = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 10) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    
+                    TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+
+                    DatePicker("Birthday", selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date)
+                        .datePickerStyle(.compact)
+                        .padding(.horizontal)
+                    
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding(.top, 5)
+                
+                }
+                .padding()
+                .background(Color(UIColor.systemBackground))
+            }
         }
-        .padding()
     }
 }
 
---
+++ Friends.swift
@@ -0,0 +1,18 @@
+//
+//  Friends.swift
+//  Birthday App
+//
+//  Created by Hafsa Ahmad on 11/07/2025.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
